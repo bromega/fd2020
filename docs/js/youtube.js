7557cf826987348e63de1d3050b95c83d4b41f74
bromega <info@bromega.com>
2020-05-23 00:58:17 -0400

    docs folder

---
+++ docs/js/youtube.js
@@ -0,0 +1,41 @@
+var paginate = function(pageToken) {
+    $.ajax({
+        url: "https://www.googleapis.com/youtube/v3/playlistItems",
+        async: false,
+        data: {
+            key: youtubeKey,
+            part: "contentDetails",
+            playlistId: playlistId,
+            maxResults: 5,
+            pageToken: pageToken
+        },
+        success: function (data) {
+            videos = videos.concat(data.items);
+            pageToken = data.nextPageToken;
+        }
+    });
+    return pageToken;
+};
+
+$(function () {
+    var pageToken = "";
+    var i = 0;
+    // var videos = [];
+    // while pageToken  != undefined {
+    while (i < 2) {
+        pageToken = paginate(pageToken);
+        i = i + 1;
+        console.log(pageToken, i);
+    }
+    for (v in videos) {
+        vid = videos[v].contentDetails.videoId;
+        yid = "#youtube" + v;
+        $(yid).html(`
+            <div class="youtube-box">
+                <a href="https://www.youtube.com/watch?v=${vid}&list=${playlistId}" target="_blank">
+                    <img src="http://i3.ytimg.com/vi/${vid}/maxresdefault.jpg" />
+                </a>
+            </div>
+        `);
+    };
+});
